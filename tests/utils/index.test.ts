b1cb006dc2a5d58044c1781f97c12994fef68efd
Alain Hélaïli <alain@carbonhub.io>
2024-01-29 12:13:52 +0000

    Removed this sample file

--- tests/utils/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { myUtilityFunction } from '../../src/utils/index';
-
-describe('Utility Functions', () => {
-  test('myUtilityFunction', () => {
-    const result = myUtilityFunction('test input');
-    expect(result).toBe('expected output');
-  });
-});
